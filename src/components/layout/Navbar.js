a515bfc9ad37764a3e6fce54561f22fc95ec9e36
Musab Abdullah <musababdullah6231@gmail.com>
2020-03-26 21:47:52 -0500

    initial commit

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+class Navbar extends Component {
+    constructor(props) {
+        super(props);
+        this.toggleNavbar = this.toggleNavbar.bind(this);
+        this.state = {
+            collapsed: true,
+        };
+    }
+    toggleNavbar() {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    }
+    render() {
+        const collapsed = this.state.collapsed;
+        const classOne = collapsed ? 'collapse navbar-collapse' : 'collapse navbar-collapse show';
+        const classTwo = collapsed ? 'navbar-toggler navbar-toggler-right collapsed' : 'navbar-toggler navbar-toggler-right';
+        return (
+            <nav className="navbar navbar-expand-sm navbar-dark bg-dark transparent-nav">
+                <div className="container">
+                    <NavLink className="navbar-brand" to="/">Study Mode</NavLink>
+                    <button onClick={this.toggleNavbar} className={`${classTwo}`} type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon" />
+                    </button>
+                    <div className={`${classOne}`} id="navbarResponsive">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item active">
+                                <NavLink className="nav-link" to="/about">About</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/contact">Contact</NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        );
+    }
+}
+export default Navbar;
