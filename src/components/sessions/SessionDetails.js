0375e4b561e059176f0b1334be0ddd395eafb659
Musab Abdullah <musababdullah6231@gmail.com>
2020-03-28 17:40:17 -0500

    updated session deets

--- src/components/sessions/SessionDetails.js
+++ src/components/sessions/SessionDetails.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 const SessionDetails = props => {
     return (
-        <div className="container">
+        <div className="container mt-5">
             <h1>Session Details</h1>
             <div className="container mt-5">
-                <p>Who: ut squaaa</p>
-                <p>Where: pcl 5th</p>
-                <p>What: compooter science</p>
-                <p>When: 24:00</p>
+                <p><b>Who:</b> ut squaaa</p>
+                <p><b>Where:</b> pcl 5th</p>
+                <p><b>What:</b> compooter science</p>
+                <p><b>When:</b> 24:00</p>
             </div>
         </div>
     )
